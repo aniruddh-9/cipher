cb852acced66484fb7a4dc37b1b3d5d17e287777
aniruddh-9 <aniruddh9.m@gmail.com>
2019-08-19 22:17:38 +0530

    attack added to caeser cipher

--- caeser-cipher.cpp
+++ caeser-cipher.cpp
@@ -3,9 +3,11 @@
  using namespace std;
 
 void getInput(vector<char>&);
-vector<char> encipher(vector<char>&);
-vector<char> decipher(vector<char>&);
+void encipher(vector<char>&,vector<char>&);
+void decipher(vector<char>&,vector<char>&,int);
 void print(vector<char>&);
+void bruteForce(vector<char>&,vector<char>&);
+void knownPlainText(vector<char>&,vector<char>&);
 
 int key = 10;
 
@@ -30,38 +32,62 @@ void print(vector<char>& msg){
     return;
 }
 
-vector<char> encipher(vector<char>& msg){
-    vector<char> cipher;
+void encipher(vector<char>& msg,vector<char>& cipher){
     for(int i=0;i<msg.size();i++){
         if(msg[i]=='\n' || msg[i]=='\t' || msg[i]==' ')
             cipher.push_back(msg[i]);
         else
-            cipher.push_back(msg[i]+key);        
+            cipher.push_back((msg[i]-97+key)%26+97);        
     }
-    return cipher;
+    return;
 }
 
-vector<char> decipher(vector<char>& cipher){
-    vector<char> msg;
+void decipher(vector<char>& cipher,vector<char>& msgd,int key){
     for(int i=0;i<cipher.size();i++){
         if(cipher[i]=='\n' || cipher[i]=='\t' || cipher[i]==' ')
-            msg.push_back(cipher[i]);
-        else
-            msg.push_back(cipher[i]-key);        
+            msgd.push_back(cipher[i]);
+        else{
+            int temp = cipher[i]-key-97;
+            if(temp<0)
+                temp+=26;
+            msgd.push_back((temp)%26+97);        
+        }
     }
-    return msg;
+    return;
+}
+
+void bruteForce(vector<char>& cipher,vector<char>&msg){
+    vector<char> msgb;
+    for(int i=0;i<26;i++){
+        decipher(cipher,msgb,i);
+        if(msgb==msg){
+            cout<<"Key : "<<i<<"\n";
+            break;
+        }
+        msgb.clear();
+    }
+}
+
+void knownPlainText(vector<char>& cipher,vector<char>& plain){
+    int k;
+    k=(int)cipher[0]-(int)plain[0];
+    cout<<"Key : "<<k<<"\n";
 }
 
  int main(){
     vector<char> msg;
     vector<char> cipher;
-    vector<char> msg1;
+    vector<char> msgd;
     getInput(msg);
-    cipher = encipher(msg);
+    encipher(msg,cipher);
     cout<<"Cipher: ";
     print(cipher);
-    msg1=decipher(cipher);
+    decipher(cipher,msgd,key);
     cout<<"Decipher: ";
-    print(msg1);
+    print(msgd);
+    cout<<"---- BruteForce ---- \n";
+    bruteForce(cipher,msg);
+    cout<<" ------ Known Plain Text ------ \n";
+    knownPlainText(cipher,msg);
     return 0;
  }
