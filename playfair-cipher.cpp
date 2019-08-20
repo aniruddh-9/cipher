abf468a5f59b50a78e4c4f9dae9e16e8c8fe4227
aniruddh-9 <aniruddh9.m@gmail.com>
2019-08-20 18:59:41 +0530

    playfair enciphering corrected

--- playfair-cipher.cpp
+++ playfair-cipher.cpp
@@ -2,17 +2,16 @@
 #include<vector>
 using namespace std;
 
-const char encoder[5][5]={{'E','Q','U','I','N'},        // EQUINOX IS THE KEY
-                    {'O','X','A','B','C'},
-                    {'D','F','G','H','K'},
-                    {'L','M','P','R','S'},
-                    {'T','V','W','Y','Z'}};
+int flag=0;
+const char encoder[5][5]=  {{'E','Q','U','I','N'},        // EQUINOX IS THE KEY
+                            {'O','X','A','B','C'},
+                            {'D','F','G','H','K'},
+                            {'L','M','P','R','S'},
+                            {'T','V','W','Y','Z'}};
 
 void getPosition(char,int&,int&);
 void sameColoumn(vector<char>&,int,int,int);
 void sameRow(vector<char>&,int,int,int);
-void sameColoumnDecipher(vector<char>&,int,int,int);
-void sameRowDecipher(vector<char>&,int,int,int);
 void diffColoumn(vector<char>&,int,int,int,int);
 void declutter(vector<char>&,vector<char>&);
 void getInput(vector<char>&);               // & required for reference
@@ -21,30 +20,28 @@ vector<char> decipher(vector<char>&);
 void print(vector<char>&);
 
 void getInput(vector<char>& msg){
-    char c;
-    cout<<"Enter the message:\n";
+    char ch;
     while(1){
-        c=getchar();
-        if (c=='j')
-            c='I';
-        else if(c>=97 && c<=122)
-            c-=32;
-        else if(c==' ')
-            continue;
-        else if(c=='\n')
+        ch=getchar();
+        if(ch=='\n')
             break;
-        else if(c=='J')
-            c='I';
-        else if(!(c>=65 && c<=90))
-            continue;
-        msg.push_back(c);
+        if(ch>=97 && ch<=122)
+            ch-=32;
+        if(ch=='J')
+            ch='I';
+        msg.push_back(ch);
     }
-    return;
 }
 
 void declutter(vector<char>& msg, vector<char>& msgx){
+    int count=0;
     for(int i=0;i<msg.size();i++){
-        if(msg[i]==msg[i+1]){
+        if(msg[i]==' '){
+            count++;
+            msgx.push_back(' ');
+            continue;
+        }
+        else if(msg[i]==msg[i+1]){
             msgx.push_back(msg[i]);
             msgx.push_back('X');
         }
@@ -52,7 +49,7 @@ void declutter(vector<char>& msg, vector<char>& msgx){
             msgx.push_back(msg[i]);
         }
     }
-    if(msgx.size()%2)
+    if((msgx.size()-count)%2)
         msgx.push_back('X');
     return;
 }
@@ -82,16 +79,25 @@ vector<char> encipher(vector<char>& msgx){
     int r1,r2,c1,c2;
     int i=0;
     while(i<msgx.size()){
+        if(msgx[i]==' '){
+            cipher.push_back(' ');
+            i++;
+            continue;
+        }
         getPosition(msgx[i],r1,c1);
         i++;
+        if(msgx[i]==' '){
+            flag=1;
+            i++;
+        }
         getPosition(msgx[i],r2,c2);
+        i++;
         if(r1==r2)
             sameRow(cipher,r1,c1,c2);
         else if(c1==c2)
             sameColoumn(cipher,c1,r1,r2);
         else
-            diffColoumn(cipher,r1,r2,c1,c2);  
-        i++;      
+            diffColoumn(cipher,r1,r2,c1,c2);      
     }
     return cipher;
 
@@ -99,51 +105,32 @@ vector<char> encipher(vector<char>& msgx){
 
 void sameRow(vector<char>& cipher,int r1,int c1,int c2){
     cipher.push_back(encoder[r1][(c1+1)%5]);
+    if (flag){
+            cipher.push_back(' ');
+            flag=0;
+        }
     cipher.push_back(encoder[r1][(c2+1)%5]);
     return;
 }
 
 void sameColoumn(vector<char>& cipher,int c1,int r1,int r2){
     cipher.push_back(encoder[(r1+1)%5][c1]);
+    if (flag){
+            cipher.push_back(' ');
+            flag=0;
+        }
     cipher.push_back(encoder[(r2+1)%5][c1]);
-    return;
-}
-
-void sameRowDecipher(vector<char>& cipher,int r1,int c1,int c2){
-    cipher.push_back(encoder[r1][(c1-1)%5]);
-    cipher.push_back(encoder[r1][(c2-1)%5]);
-    return;
-}
-
-void sameColoumnDecipher(vector<char>& cipher,int c1,int r1,int r2){
-    cipher.push_back(encoder[(r1-1)%5][c1]);
-    cipher.push_back(encoder[(r2-1)%5][c1]);
     return;
 }
 
 void diffColoumn(vector<char>& cipher,int r1,int r2,int c1,int c2){
     cipher.push_back(encoder[r1][c2]);
+    if (flag){
+            cipher.push_back(' ');
+            flag=0;
+        }
     cipher.push_back(encoder[r2][c1]);
     return;
-}
-
-vector<char> decipher(vector<char>& cipher){
-    vector<char> decipher;
-    int r1,r2,c1,c2;
-    int i=0;
-    while(i<cipher.size()){
-        getPosition(cipher[i],r1,c1);
-        i++;
-        getPosition(cipher[i],r2,c2);
-        if(r1==r2)
-            sameRowDecipher(decipher,r1,c1,c2);
-        else if(c1==c2)
-            sameColoumnDecipher(decipher,c1,r1,r2);
-        else
-            diffColoumn(decipher,r1,r2,c1,c2);  
-        i++;      
-    }
-    return decipher;
 }
 
 int main(){
@@ -152,12 +139,14 @@ int main(){
     vector<char> cipher;
     vector<char> msgd;
     getInput(msg);
+    print(msg);
     declutter(msg,msgx);
+    print(msgx);
     cipher = encipher(msgx);
     cout<<"Cipher Text: ";
     print(cipher);
-    cout<<"Decipher Text: ";
-    msgd = decipher(cipher);
-    print(msgd);
+   // cout<<"Decipher Text: ";
+   // msgd = decipher(cipher);
+   // print(msgd);
     return 0;
 }
