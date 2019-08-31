e33db7c9e829a78edfb0de6009c01772ee19f304
aniruddh-9 <aniruddh9.m@gmail.com>
2019-08-31 20:22:11 +0530

    RC4 stream generation added

--- RC4.cpp
+++ RC4.cpp
@@ -2,13 +2,61 @@
 #include<vector>
 using namespace std;
 
+int S[256],T[256];
+
 void getInput();
 void KSA();
 void PRGA();
 void encipher();
+void initialize(int[],int);
+void initPermutation();
+void streamGenerate();
 
+void swap(int& a,int& b){
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+}
+
+void initialize(int key[],int keylen){
+    for(int i=0;i<256;i++){
+        S[i]=i;
+        T[i]=key[i%keylen];
+    }
+}
+
+void initPermutation(){
+    int j=0;
+    for(int i=0;i<256;i++){
+        j=(j+S[i]+T[i])%256;
+        swap(S[i],S[j]);
+    }
+}
+
+void streamGenerate(){
+    int t;
+    int i=0;
+    int j=0;
+    int k;
+    while(true)
+        i=(i+1)%256;
+    j=(j+S[i])%256;
+    swap(S[i],S[j]);
+    t=(S[i]+S[j])%256;
+    k=S[t];
+}
 
 int main(){
-    getInput();
+    int keylen;
+    cout<<"Enter Key Length: ";
+    cin>>keylen;
+    int key[keylen];
+    cout<<"Enter Key: ";
+    for(int i=0;i<keylen;i++)
+        cin>>key[i];
+    initialize(key,keylen);
+    initPermutation();
+    streamGenerate();
     return 0;
 }
